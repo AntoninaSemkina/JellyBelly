9ce33d0cd8642d6f653b1d3b29f6a04624342153
Antonina Semkina <antonina.a.semkina@gmail.com>
2024-10-29 23:25:40 +0300

    lesson 8

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import "./App.css";
-import { useState } from "react";
+import { createContext, useState, Dispatch } from "react";
 import Header from "./components/Header/";
 import Cards from "./components/Cards";
 import Search from "./components/Search";
 
+type ContextType = {
+  theme: string;
+  setTheme: Dispatch<React.SetStateAction<string>>;
+};
+
+export const ThemeContext = createContext<ContextType | null>(null);
+
 function App() {
   const [filterValue, setFilterValue] = useState("");
-  console.log("filterValue", filterValue);
 
+  const [theme, setTheme] = useState("dark");
   return (
-    <div className="container">
-      <Header />
-      <Search setFilterValue={setFilterValue} />
-      <Cards filterValue={filterValue} />
-    </div>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className={`container ${theme}`}>
+        <Header />
+        <Search setFilterValue={setFilterValue} />
+        <Cards filterValue={filterValue} />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,14 +1,16 @@
-import style from "./style.module.css"
-const Nav = () =>{
-    return(
-        <nav className={style.container}>
-                <a>Beans</a>
-                <a>Facts</a>
-                <a>Recipes</a>
-                <a>Combinations</a>
-                <a>History</a>
-        </nav>
-    )
-}
+import SwitchTheme from "../SwitchTheme";
+import style from "./style.module.css";
+const Nav = () => {
+  return (
+    <nav className={style.container}>
+      <a>Beans</a>
+      <a>Facts</a>
+      <a>Recipes</a>
+      <a>Combinations</a>
+      <a>History</a>
+      <SwitchTheme />
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
